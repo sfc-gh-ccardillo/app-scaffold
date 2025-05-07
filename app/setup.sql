7438e99a6d84baacab95079d070a3ac111ede1dd
sfc-gh-ccardillo <ccardillo@snowflake.com>
2025-05-07 11:54:38 -0400

    start grant callback

--- app/setup.sql
+++ app/setup.sql
@@ -1,10 +1,50 @@
+-- App Roles
 CREATE APPLICATION ROLE IF NOT EXISTS app_public;
 
+-- App Schemas
 CREATE OR ALTER VERSIONED SCHEMA core;
 GRANT USAGE ON SCHEMA core TO APPLICATION ROLE app_public;
 
+CREATE OR ALTER VERSIONED SCHEMA setup;
+
+-- Streamlit
 CREATE OR REPLACE STREAMLIT core.ui
      FROM '/streamlit/'
      MAIN_FILE = 'ui.py';
 
 GRANT USAGE ON STREAMLIT core.ui TO APPLICATION ROLE app_public;
+
+-- Create Services
+CREATE OR REPLACE PROCEDURE setup.create_services(privileges ARRAY)
+RETURNS VARCHAR 
+LANGUAGE SQL
+EXECUTE AS OWNER
+AS $$
+    BEGIN
+        LET pool_name := (SELECT current_database()) || '_app_pool';
+
+        create compute pool if not exists identifier(:pool_name)
+            MIN_NODES = 1
+            MAX_NODES = 1
+            INSTANCE_FAMILY = CPU_X64_M;
+
+     --    create service if not exists services.spcs_na_service
+     --        in compute pool identifier(:pool_name)
+     --        from spec='service_spec.yml';
+
+     --    grant usage on service services.spcs_na_service
+     --        to application role app_public;
+
+     --    create or replace function services.echo(payload varchar)
+     --        returns varchar
+     --        service = services.spcs_na_service
+     --        endpoint = 'my-endpoint'
+     --        max_batch_rows = 50
+     --        AS '/echo';
+
+     --    grant usage on function services.echo(varchar)
+     --        to application role app_public;
+
+        return 'Done';
+    end;
+$$;
--- streamlit/ui.py
+++ streamlit/ui.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 from snowflake.snowpark import Session
 import snowflake.permissions as permissions
@@ -10,7 +11,7 @@ current_org = session.sql("SELECT CURRENT_ORGANIZATION_NAME() AS result").collec
 current_acct = session.sql("SELECT CURRENT_ACCOUNT_NAME() AS result").collect()[0].RESULT
 current_db = session.sql("SELECT CURRENT_DATABASE() AS result").collect()[0].RESULT
 
-# Privileges check
+# Privileges Check
 st.header("Privileges Check")
 app_url_base = f"https://app.snowflake.com/{current_org}/{current_acct}/#/apps/application/{current_db}"
 privileges_url = app_url_base + "/security/privileges"
@@ -28,3 +29,14 @@ else:
     st.text("Some privileges required to run this app have not been granted.")
     st.html(f'Please use the <a href="{privileges_url}" target="_blank" >privileges panel</a> to grant the following privileges:')
     [privilege for privilege, granted in privileges_check.items() if not granted]
+
+# Compute Pools
+st.header("Compute Pools")
+
+compute_pools = session.sql("SHOW COMPUTE POOLS").collect()
+if compute_pools:
+    compute_pools_df = pd.DataFrame(compute_pools)
+    compute_pools_df = compute_pools_df[["name", "state", "num_services", "num_jobs", "active_nodes", "idle_nodes", "target_nodes"]]
+    st.dataframe(compute_pools_df, hide_index=True)
+else:
+    st.text("No compute pools have not been created yet. Please ensure account permissions have been granted.")
